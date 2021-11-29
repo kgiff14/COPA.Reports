2e2c524ac07cd9dde8d91a1530577d44f1f24115
kordellgifford <kordellgifford@conservice.com>
2021-11-29 15:42:48 -0700

    Add project files.

---
+++ COPA.Reports/BLL/ArangoAccessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COPA.Reports.BLL
+{
+    public class ArangoAccessor
+    {
+        public readonly CollectionConnector
+    }
+}
